Fixed pixel cost computation and parameter passing to functions.

Image Denoising/generate.py
@@ -12,11 +12,10 @@ def generate_image(
         size=size
     )
 
+    for _ in range(gen_iter):
+        image = step(image, labels, eps, beta)
 
-    for i in range(gen_iter):
-        image = step(image, labels)
-
-    noise = np.random.rand(*image.shape) < eps
+    noise = (np.random.rand(*image.shape) < eps).astype(int)
     noised_image = np.logical_xor(image, noise)
 
     plt.imshow(image, cmap='gray')

Image Denoising/gibbs_sampler.py
@@ -6,15 +6,15 @@ from graph import (
 )
 import numpy as np
 
-def sample_pixel(i, j, image, labels):
+def sample_pixel(i, j, image, labels, eps, beta):
     probs = []
+    neighbour_pxls = neighbours(image, i, j)
     for label in labels:
         pixel = Pixel(i, j, label)
-        pxl_cost = pixel_cost(pixel, label)
-        neighbour_pxls = neighbours(image, pixel)
+        pxl_cost = pixel_cost(pixel, image[i, j], eps)
         neighbours_costs = []
         for neighbour in neighbour_pxls:
-            neighbours_costs += [edge_cost(pixel, neighbour)]
+            neighbours_costs += [edge_cost(pixel, neighbour, beta)]
 
         probs.append(np.exp(-pxl_cost - np.sum(neighbours_costs)))
 
@@ -23,10 +23,10 @@ def sample_pixel(i, j, image, labels):
     return sampled_pixel
 
 
-def step(image, labels):
+def step(image, labels, eps, beta):
     for i in range(image.shape[0]):
         for j in range(image.shape[1]):
-            image[i, j] = sample_pixel(i, j, image, labels)
+            image[i, j] = sample_pixel(i, j, image, labels, eps, beta)
 
     return image
 

Image Denoising/graph.py
@@ -13,8 +13,7 @@ def edge_cost(pixel1, pixel2, beta=0.7):
     else:
         return 0
 
-def neighbours(image, pixel):
-    i, j, _ = pixel
+def neighbours(image, i, j):
     padding = (-1) * np.ones(
         (image.shape[0] + 2, 
          image.shape[1] + 2)

Image Denoising/main.py
@@ -34,16 +34,19 @@ def main(settings_file):
     )
 
     print('Starting sampling...')
-    frequences = np.zeros(input_image.shape)
+    zero_frequences = np.zeros(input_image.shape)
+    ones_frequences = np.zeros(input_image.shape)
     j = 0
     for i in range(sampling_iter):
-        input_image = step(input_image, labels)
+        input_image = step(input_image, labels, eps, beta)
         if i > drop and i % save_step == 0:
-            frequences += np.logical_xor(input_image, np.ones(input_image.shape))
+            zero_frequences += np.logical_xor(input_image, np.ones(input_image.shape))
+            ones_frequences += np.logical_xor(input_image, np.zeros(input_image.shape))
             j += 1
 
-    result_image = frequences < (j // 2) * np.ones(frequences.shape)
+    result_image = ones_frequences > zero_frequences
     plt.imshow(result_image, cmap='gray')
+    plt.title('Result image')
     plt.savefig('result')
 
     